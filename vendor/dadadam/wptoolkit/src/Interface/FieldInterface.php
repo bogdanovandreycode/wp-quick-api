78342905c94da68b8f03a6db4af68ec46a5d2198
Dadadam <bogdanov.andrey.code@gmail.com>
2024-01-12 23:45:49 +0200

    Delete vendor

--- vendor/dadadam/wptoolkit/src/Interface/FieldInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WpToolKit\Interface;
-
-interface FieldInterface
-{
-    public function renderLabel(): string;
-    public function renderField(): string;
-}
--- vendor/dadadam/wptoolkit/src/Interface/MetaBoxInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WpToolKit\Interface;
-
-interface MetaBoxInterface
-{
-    public function render($post): void;
-    public function callback($postId): void;
-}
