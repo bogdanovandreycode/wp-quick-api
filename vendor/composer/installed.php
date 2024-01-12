c6d0079012ae9a5abb1ddece60be0edf594446b4
Dadadam <bogdanov.andrey.code@gmail.com>
2024-01-12 14:29:55 +0200

    Update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dadadam/wp-quick-api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5f92c0e306a66e005f0fbd50eba63ad8a70d7f6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dadadam/wp-quick-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5f92c0e306a66e005f0fbd50eba63ad8a70d7f6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
